f45ff0972606b8102c069f7d9728c368c9b756d9
MM7mDM <111407171+MM7mDM@users.noreply.github.com>
2022-11-15 17:18:23 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made by @0pskw
+      M7Plus Studio  حقوق الطبع والنشر 
       </footer>
     </>
   )
