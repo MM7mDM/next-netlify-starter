13e5b296f12f3c183095f2339fc33dc2fd0a3875
MM7mDM <111407171+MM7mDM@users.noreply.github.com>
2023-08-04 13:53:16 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>M7Plus</title>
+        <title>استمتع</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
